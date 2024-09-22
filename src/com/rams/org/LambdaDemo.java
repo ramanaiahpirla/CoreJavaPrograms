f78051a4dfe64b03238954d5b6fc0e26dac5c1d5
RamanaiahPirla <ramanaiah.237@gmail.com>
2024-09-22 13:17:32 +0530

    updated code

--- src/com/rams/org/LambdaDemo.java
+++ src/com/rams/org/LambdaDemo.java
@@ -1,18 +1,17 @@
 package com.rams.org;
 
+import java.util.function.BiConsumer;
+
 public class LambdaDemo {
 
 	public static void main(String[] args) {
-		LambdaDemo demo = new LambdaDemo();
-		demo.add(10, 20);
-
+		BiConsumer<Integer, Integer> biConsumer = (a,b) -> System.out.println(a+b);
+		biConsumer.accept(10, 20);
 		
-		
+	
+		BiConsumer<Integer, Integer> response = (a,b) -> System.out.println(a * b);
+		response.accept(10, 20);
 	}
 	
-	public void add(int a, int b) {
-		System.out.println(a+b);
-		
-	}
 
 }
