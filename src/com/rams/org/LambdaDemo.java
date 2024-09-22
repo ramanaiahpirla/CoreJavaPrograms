a56ff411abc7df2496267f33e82d7b23b8ba4c41
RamanaiahPirla <ramanaiah.237@gmail.com>
2024-09-22 12:33:50 +0530

    added sample code

--- src/com/rams/org/LambdaDemo.java
+++ src/com/rams/org/LambdaDemo.java
@@ -3,7 +3,13 @@ package com.rams.org;
 public class LambdaDemo {
 
 	public static void main(String[] args) {
+		LambdaDemo demo = new LambdaDemo();
+		demo.add(10, 20);
 
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
 	}
 
 }
