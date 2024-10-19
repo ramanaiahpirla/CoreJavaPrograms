04a30e8fe6897ed44410c48e40ce52e6451fe5e2
RamanaiahPirla <ramanaiah.237@gmail.com>
2024-10-19 16:26:52 +0530

    updated code to find frequency of each char

--- src/com/rams/org/FrequencyOfEachCharInGivenString.java
+++ src/com/rams/org/FrequencyOfEachCharInGivenString.java
@@ -1,8 +1,15 @@
 package com.rams.org;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FrequencyOfEachCharInGivenString {
 	public static void main(String[] args) {
 		
+		String line = "Ram is employee of ABC company, ram is from Blore, RAM! is good in algorithms.";
+		Map<Character, Long> eachCharFrequency =  line.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		System.out.println(eachCharFrequency);
 	}
 
 }
